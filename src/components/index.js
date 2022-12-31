bc978246e2bf499fe1b636205ccb8d1f7b2c2aea
Mashfikur rahman <md.mashfikur1@gmail.com>
2022-12-31 22:51:31 +0600

    deploy done

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import GetStarted from "./GetStarted";
+import Footer from "./Footer";
+
+
+export {
+    Navbar, Billing, Business, Button, 
+    CardDeal, FeedbackCard, Clients, 
+    CTA, Hero, Stats, Testimonials, 
+    GetStarted, Footer
+};
